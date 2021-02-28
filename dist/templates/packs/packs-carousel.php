d0fc1b32d625d35eb8e8be26ac820d547f193161
Leandro Videla <leandrovidelamiranda@gmail.com>
2021-02-28 18:26:43 -0300

    agrego packs

--- dist/templates/packs/packs-carousel.php
+++ dist/templates/packs/packs-carousel.php
@@ -1,24 +1,16 @@
 <div class="col packs-owl-carousel-column">
   <div class="owl-carousel" id="packs-owl-carousel">
   <article class="item Pack-card">
-      <img src="content-cities/<?=$orange->get('_sublanding');?>/<?=$orange->get('precio-pack-1');?>" class="img-fluid"
+      <img src="assets/img/packs/paquetes-01.png" class="img-fluid"
         alt="Pack">
-      <a href="#form-promo" class="btn-card btn-g btn-blue-outline go-to">contratar</a>
     </article>
     <article class="item Pack-card">
-      <img src="content-cities/<?=$orange->get('_sublanding');?>/<?=$orange->get('precio-pack-2');?>" class="img-fluid"
+      <img src="assets/img/packs/paquetes-02.png"
         alt="Pack">
-      <a href="#form-promo" class="btn-card btn-g btn-blue-outline go-to">contratar</a>
     </article>
     <article class="item Pack-card">
-      <img src="content-cities/<?=$orange->get('_sublanding');?>/<?=$orange->get('precio-pack-3');?>" class="img-fluid"
+      <img src="assets/img/packs/paquetes-03.png"
         alt="Pack">
-      <a href="#form-promo" class="btn-card btn-g btn-blue-outline go-to">contratar</a>
-    </article>
-    <article class="item Pack-card">
-      <img src="content-cities/<?=$orange->get('_sublanding');?>/<?=$orange->get('precio-pack-4');?>" class="img-fluid"
-        alt="Pack">
-      <a href="#form-promo" class="btn-card btn-g btn-blue-outline go-to">contratar</a>
     </article>
   </div>
 </div>
--- dist/templates/packs/packs.php
+++ dist/templates/packs/packs.php
@@ -1,41 +1,22 @@
 <div class="Packs" id="packs">
   <div class="container">
-    <div class="row">
-      <div class="col Packs__wrapper-titles">
-        <h1 class="Packs__title" data-aos="fade-up">
-          Elige tu triple pack <span>de Megacable</span>
-        </h1>
-        <h4 class="Packs__subtitle" data-aos="fade-up" data-aos-delay="100">Cantidad de megas, llamadas y canales en HD.
-          <span>¡Tú eliges!</span></h4>
-      </div>
-    </div>
     <div class="row Packs__prices">
     <article class="col-lg-3 col-md-6 packs-desktop">
-        <a href="#form-promo" class="Pack-card go-to cards-packs">
-          <img src="content-cities/<?=$orange->get('_sublanding');?>/<?=$orange->get('precio-pack-1');?>"
+        <a href="#hero" class="Pack-card go-to cards-packs">
+          <img src="assets/img/packs/paquetes-01.png"
             class="img-fluid" alt="Pack">
-          <div class="btn-card btn-g btn-blue-filled">contratar</div>
         </a>
       </article>
       <article class="col-lg-3 col-md-6 packs-desktop">
-        <a href="#form-promo" class="Pack-card go-to cards-packs">
-          <img src="content-cities/<?=$orange->get('_sublanding');?>/<?=$orange->get('precio-pack-2');?>"
+        <a href="#hero" class="Pack-card go-to cards-packs">
+          <img src="assets/img/packs/paquetes-02.png"
             class="img-fluid" alt="Pack">
-          <div class="btn-card btn-g btn-blue-filled">contratar</div>
         </a>
       </article>
       <article class="col-lg-3 col-md-6 packs-desktop">
-        <a href="#form-promo" class="Pack-card go-to cards-packs">
-          <img src="content-cities/<?=$orange->get('_sublanding');?>/<?=$orange->get('precio-pack-3');?>"
+        <a href="#hero" class="Pack-card go-to cards-packs">
+          <img src="assets/img/packs/paquetes-03.png"
             class="img-fluid" alt="Pack">
-          <div class="btn-card btn-g btn-blue-filled">contratar</div>
-        </a>
-      </article>
-      <article class="col-lg-3 col-md-6 packs-desktop">
-        <a href="#form-promo" class="Pack-card go-to cards-packs">
-          <img src="content-cities/<?=$orange->get('_sublanding');?>/<?=$orange->get('precio-pack-4');?>"
-            class="img-fluid" alt="Pack">
-          <div class="btn-card btn-g btn-blue-filled">contratar</div>
         </a>
       </article>
       <?php include 'packs-carousel.php';?>
